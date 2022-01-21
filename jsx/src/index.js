7b526536598fe8ae2cfc79815ac77f3a716b6281
Pirci <emrepirci@gmail.com>
2022-01-21 15:31:44 +0100

    Create component and show

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,7 +1,11 @@
 // *Import the React and ReactDOM libraries.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// *Create a React component.
+const App = () => {
+	return <div>Hi there!</div>;
+};
 
-// *Create a react component.
-
-
-// *Take the react component and show it on the screen.
+// *Take the React component and show it on the screen.
+ReactDOM.render(<App />, document.querySelector('#root'));
