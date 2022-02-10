db2d4cad53333f96a0dfc3c531c8f1aee6b80247
Pirci <emrepirci@gmail.com>
2022-02-10 14:45:49 +0100

    onClick event

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
+	state = {
+		categories: [
+			{ categoryId: 1, categoryName: 'Beverages' },
+			{ categoryId: 2, categoryName: 'Condiments' },
+		],
+	};
+
 	render() {
 		return (
 			<div>
 				<h3>{this.props.info.title}</h3>
 				<ListGroup>
-					<ListGroupItem>Cras justo odio</ListGroupItem>
-					<ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-					<ListGroupItem>Morbi leo risus</ListGroupItem>
-					<ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-					<ListGroupItem>Vestibulum at eros</ListGroupItem>
+					{this.state.categories.map((category) => (
+						<ListGroupItem>{category.categoryName}</ListGroupItem>
+					))}
 				</ListGroup>
 			</div>
 		);
